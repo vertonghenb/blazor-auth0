b426d22e137b974b8169b91709da69c4bac1604a
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2024-10-03 11:07:46 +0200

    Initial Commit

---
+++ Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private static readonly List<WeatherForecast> forecasts = new();
+
+    static WeatherForecastController()
+    {
+        forecasts.AddRange(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }));
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> GetForecasts()
+    {
+        return forecasts;
+    }
+
+    [HttpPost]
+    public WeatherForecast CreateForecast(WeatherForecast forecast)
+    {
+        forecasts.Add(forecast);
+        return forecast;
+    }
+}
