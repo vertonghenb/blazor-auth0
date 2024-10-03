4e1ca94bcffc8f1ff8f46520f4d308eab4d17999
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2024-10-03 12:34:31 +0200

    Client Auth

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,4 +8,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.PostLogoutRedirectUri = builder.HostEnvironment.BaseAddress;
+});
+
 await builder.Build().RunAsync();
